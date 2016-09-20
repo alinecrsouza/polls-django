4204ff92fa4293994b33e6cec02e26f72c67cca3
alinecrsouza <linecrsouza@gmail.com.br>
2016-09-20 08:56:50 -0300

    Building the Detail view and dealing with HTTP404 exceptions

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 
@@ -28,8 +28,22 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
+'''
+# use get() and raise Http404 if the object doesn’t exist
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+'''
+
+# using the shortcut: get_object_or_404()
+# There’s also a get_list_or_404() function, which works just as get_object_or_404()
+# – except using filter() instead of get(). It raises Http404 if the list is empty.
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
