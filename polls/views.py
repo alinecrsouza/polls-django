b9073689501d94ca987e351a37b37652819187e7
alinecrsouza <linecrsouza@gmail.com.br>
2016-09-23 09:29:58 -0300

    Using generic views

--- polls/views.py
+++ polls/views.py
@@ -3,53 +3,28 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.template import loader
 from django.db.models import F
+from django.views import generic
 
 from .models import Choice, Question
 
-'''
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-'''
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-'''
-# load a template, fill a context and return an HttpResponse
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
-'''
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-# using the shortcut: render() to load a template, fill a context and return an HttpResponse
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
 
-'''
-# use get() and raise Http404 if the object doesn’t exist
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
-'''
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-# using the shortcut: get_object_or_404()
-# There’s also a get_list_or_404() function, which works just as get_object_or_404()
-# – except using filter() instead of get(). It raises Http404 if the list is empty.
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
