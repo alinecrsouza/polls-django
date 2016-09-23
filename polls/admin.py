10835719f742a7d52d4fea144853df261df0a660
alinecrsouza <linecrsouza@gmail.com.br>
2016-09-23 11:37:02 -0300

    Inserting CSS | Reordering Fields | Spliting the form up into fieldsets

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+'''
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
+'''
+
+# split the form up into fieldsets
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
